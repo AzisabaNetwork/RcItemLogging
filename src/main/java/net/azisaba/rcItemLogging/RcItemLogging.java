5d39bd7b4116690162d55b5880afa858e054cabe
sysnote8main <sysnote8main@gmail.com>
2025-02-08 08:05:12 +0900

    chore: remove debug code for API write testing

--- src/main/java/net/azisaba/rcItemLogging/RcItemLogging.java
+++ src/main/java/net/azisaba/rcItemLogging/RcItemLogging.java
@@ -14,20 +14,6 @@ public final class RcItemLogging extends JavaPlugin {
         getLogger().info("Data folder path: " + getDataPath());
         RcItemLoggingAPI.init(getLogger(), dataFolder);
 
-        // log test code
-        UUID playerFrom = UUID.randomUUID();
-        UUID playerTo = UUID.randomUUID();
-        for(int i=0;i<15;i++) {
-            RcItemLoggingAPI.put(
-                    "welcome" + i,
-                    "minecraft:stone",
-                    playerFrom,
-                    playerTo,
-                    null
-            );
-            getLogger().info("Completed: " + i);
-        }
-
         getLogger().info("Initialized!");
     }
 
